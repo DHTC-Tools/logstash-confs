34c923a1ba8b94001079e28ce3a7a650b0490b22
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-06 17:20:09 -0500

    Add script to filter out logs

---
+++ job-logs/python/process_logs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+
+csv_re = re.compile(r'(.*)\.csv')
+for logfile in os.listdir('.'):
+    if not logfile.endswith('.csv'):
+        continue
+    filename = csv_re.match(logfile).group(1)
+    output = open('./{0}-corrected.csv'.format(filename), 'w')
+    for line in open(logfile, 'r'):
+        if len(line.split(',')) != 87:
+            sys.stdout.write(line)
+        else:
+            output.write(line)
