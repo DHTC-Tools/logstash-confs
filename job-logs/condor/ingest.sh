9c75f361e212dfc6decca0250ae9ce456c780b34
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-07 18:13:14 -0500

    Fix typo

--- job-logs/condor/ingest.sh
+++ job-logs/condor/ingest.sh
@@ -4,6 +4,6 @@ export PATH=/bin:/usr/bin
 cur_dir=`pwd`
 ./process_logs.py $1
 sed -i "s/ES_INDEX/$2/" joblog.conf
-sed -i "s/DATESTRING/$1/" joblog.conf
+sed -i "s/DATE_STRING/$1/" joblog.conf
 sed -i "s|CUR_DIR|$cur_dir|" joblog.conf
 /opt/logstash/bin/logstash -f joblog.conf
