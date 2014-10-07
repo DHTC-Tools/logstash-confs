1c28701194cc3eb6167fe75c6eca55a7a0fc4e32
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-07 18:20:04 -0500

    Need to use stdin for logstash to get it to quit when done

--- job-logs/condor/ingest.sh
+++ job-logs/condor/ingest.sh
@@ -4,6 +4,4 @@ export PATH=/bin:/usr/bin
 cur_dir=`pwd`
 ./process_logs.py $1
 sed -i "s/ES_INDEX/$2/" joblog.conf
-sed -i "s/DATE_STRING/$1/" joblog.conf
-sed -i "s|CUR_DIR|$cur_dir|" joblog.conf
-/opt/logstash/bin/logstash -f joblog.conf
+cat jobsarchived$1.csv | /opt/logstash/bin/logstash -f joblog.conf
