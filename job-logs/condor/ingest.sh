0cc37ef690fc3f4de24e7f85c073dc65cee6236a
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-07 17:42:00 -0500

    fix path

--- job-logs/condor/ingest.sh
+++ job-logs/condor/ingest.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export PATH=/bin:/usr/bin
 cur_dir=`pwd`
 ./process_logs.py
 sed -i "s/ES_INDEX/$2/" joblog.conf
 sed -i "s/CUR_DIR/$cur_dir/g" joblog.conf
- /opt/logstash/bin/logstash -f joblog.conf
+/opt/logstash/bin/logstash -f joblog.conf
