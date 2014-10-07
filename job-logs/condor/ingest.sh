5d10ba1e760f8cf2882a42202b2094ddc8a79bf5
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-07 18:02:58 -0500

    Use | as a delimiter in sed since we're using paths

--- job-logs/condor/ingest.sh
+++ job-logs/condor/ingest.sh
@@ -4,5 +4,5 @@ export PATH=/bin:/usr/bin
 cur_dir=`pwd`
 ./process_logs.py $1
 sed -i "s/ES_INDEX/$2/" joblog.conf
-sed -i "s/CUR_DIR/$cur_dir/" joblog.conf
+sed -i "s|CUR_DIR|$cur_dir|" joblog.conf
 /opt/logstash/bin/logstash -f joblog.conf
