36ab34949d6057b57acc88afa0d21657cc2cc9bf
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-16 11:47:53 -0500

    process_logs uses -- options now

--- job-logs/condor/generate_clean_logs.sh
+++ job-logs/condor/generate_clean_logs.sh
@@ -2,4 +2,4 @@
 
 export PATH=/bin:/usr/bin
 cur_dir=`pwd`
-./process_logs.py $1 save
+./process_logs.py --date $1 --save-raw
--- job-logs/condor/ingest.sh
+++ job-logs/condor/ingest.sh
@@ -2,6 +2,6 @@
 
 export PATH=/bin:/usr/bin
 cur_dir=`pwd`
-./process_logs.py $1
+./process_logs.py --date $1
 sed -i "s/ES_INDEX/$2/" joblog.conf
 cat jobsarchived$1.csv | /opt/logstash/bin/logstash -f joblog.conf
