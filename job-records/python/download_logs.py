46fd96d07cd1126b69832c05c053a5acde942a8f
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-10-31 17:01:50 -0500

    Refactor processing to separate downloading and processing records

---
+++ job-records/python/download_logs.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import urllib2
+import argparse
+
+AMAZON_CSV_URL = "http://atlas-panda-jobsarchived.s3.amazonaws.com"
+FAXBOX_CSV_URL = "http://faxbox.usatlas.org/"
+
+
+def download_log(date_string, source=None):
+    """
+    Download job log files from Amazon EC2 machines
+
+    parameters:
+    date_string - date to start download
+    source      - string set to either faxbox or amazon to indicate where
+                  to get logs from
+    """
+    url_file = "jobsarchived{0}.csv".format(date_string)
+    if source is None:
+        return False
+    elif source.lower() == 'amazon':
+        csv_url = "{0}/{1}".format(AMAZON_CSV_URL, url_file)
+    elif source.lower() == 'faxbox':
+        csv_url = "{0}/{1}".format(FAXBOX_CSV_URL, url_file)
+    else:
+        return False
+    try:
+        request = urllib2.urlopen(csv_url)
+        if request.getcode() != 200:
+            sys.stderr.write("Can't download {0}".format(csv_url))
+            return None
+    except urllib2.HTTPError:
+        sys.stderr.write("Can't download {0}".format(csv_url))
+        return False
+    output_file = open(url_file, 'w')
+    for line in request:
+        output_file.write(line)
+    return True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Download ATLAS job records')
+    parser.add_argument('--date', dest='date', default=None, required=True,
+                        help='Date to download')
+    parser.add_argument('--source', dest='source', default=None,
+                        choices=['amazon', 'faxbox'], required=True,
+                        help='filename of input file')
+
+    args = parser.parse_args(sys.argv[1:])
+    if len(args.date) != 8:
+        sys.stderr.write("Invalid date argument: {0}\n".format(args.date))
+    try:
+        int(args.date)
+    except ValueError:
+        sys.stderr.write("Invalid date argument: {0}\n".format(args.date))
+
+    download_log(args.date, args.source)
--- job-records/python/process_logs.py
+++ job-records/python/process_logs.py
@@ -1,83 +1,60 @@
 #!/usr/bin/env python
 
 import sys
-import urllib2
 import argparse
+import re
 
-JOB_LOG_URL = "http://atlas-panda-jobsarchived.s3.amazonaws.com"
 
-
-def download_log(date_string, filename=None, save_raw=False):
+QUOTE_RE = re.compile(r'.*"[^,]*$')
+def generate_log_lines(input_file, bad_file):
     """
-    Download job log files from Amazon EC2 machines
+    Generator that processes csv with ATLAS records and try to fix broken lines
+    returns fixed lines
+
+    :param input_file: file object with input
+    :param badfile: file object to write lines with errors to
+    :return: corrected lines stripped of tabs and embedded newlines
+    """
+    buffer = ""
+    for line in input_file:
+        buffer += line
+        if QUOTE_RE.match(buffer):
+            continue
+        else:
+            record = buffer
+            buffer = ""
+            yield record
+
+
+
+def process_records(date_string):
+    """
+    Process log files and remove tabs and try to join broken lines
 
     parameters:
     date_string - date to start download
-    filename    - filename of already downloaded csv file, specifying this
-                  results in this file being used instead of a download
-    save_raw    - preserve original files?
     """
-    if filename is None:
-        url_file = "jobsarchived{0}.csv".format(date_string)
-        if save_raw:
-            csv_file = "jobsarchived{0}-cleaned.csv".format(date_string)
-            bad_filename = "jobsarchived{0}-bad.csv".format(date_string)
-        else:
-            csv_file = "jobsarchived{0}.csv".format(date_string)
-        csv_url = "{0}/{1}".format(JOB_LOG_URL, url_file)
-        try:
-            request = urllib2.urlopen(csv_url)
-            if request.getcode() != 200:
-                sys.stderr.write("Can't download {0}".format(csv_url))
-                return None
-        except urllib2.HTTPError:
-            sys.stderr.write("Can't download {0}".format(csv_url))
-            return None
-        if save_raw:
-            orig_file = open(url_file, 'w')
-            bad_file = open(bad_filename, 'w')
-    else:
-        request = open(filename, 'r')
-        if save_raw:
-            csv_file = "{0}-cleaned.csv".format(filename)
-            bad_file = open("{0}-bad.csv".format(filename), 'w')
-        else:
-            csv_file = "{0}.csv".format(filename)
+    input_file = open("jobsarchived{0}.csv".format(date_string), 'r')
+    output_file = open("jobsarchived{0}-processed.csv".format(date_string), 'w')
+    bad_file = open("jobsarchived{0}-bad.csv".format(date_string), 'w')
 
-    output_file = open(csv_file, 'w')
-    error_lines = 0
-    for line in request:
-        if save_raw and filename is None:
-            orig_file.write(line)
-        line = line.replace("\t", '')
-        if len(line.split(',')) != 87:
-            if save_raw:
-                bad_file.write(line)
-            error_lines += 1
-            continue
+    for line in generate_log_lines(input_file, bad_file):
         output_file.write(line)
-    sys.stderr.write("{0} lines skipped due to errors".format(error_lines))
     return None
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Download and process ATLAS'
-                                                 'job records')
-    parser.add_argument('--date', dest='date', default=None,
+    parser = argparse.ArgumentParser(description='Process ATLAS job records')
+    parser.add_argument('--date', dest='date', default=None, required=True,
                         help='Date to download')
-    parser.add_argument('--filename', dest='filename', default=None,
-                        help='filename of input file')
-    parser.add_argument('--save-raw', dest='save_raw',
-                        action='store_true',
-                        help='Save raw log files instead of replacing in place')
 
     args = parser.parse_args(sys.argv[1:])
-    if args.filename is None and len(args.date) != 8:
+    if len(args.date) != 8:
         sys.stderr.write("Invalid date argument: {0}\n".format(args.date))
-    if args.date is not None:
-        try:
-            int(args.date)
-        except ValueError:
-            sys.stderr.write("Invalid date argument: {0}\n".format(args.date))
 
-    download_log(args.date, args.filename, args.save_raw)
+    try:
+        int(args.date)
+    except ValueError:
+        sys.stderr.write("Invalid date argument: {0}\n".format(args.date))
+
+    process_log(args.date)
