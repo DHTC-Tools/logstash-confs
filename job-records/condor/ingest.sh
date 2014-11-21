ec88bde8c7cce61d930a82109ead1fce785f08ec
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-11-21 13:13:54 -0600

    Fix incorrect url reference for faxbox

--- job-records/condor/ingest.sh
+++ job-records/condor/ingest.sh
@@ -3,7 +3,10 @@
 export PATH=/bin:/usr/bin
 cur_dir=`pwd`
 ./download_logs.py --date $1 --source $2
-./process_logs.py --date $1
+if [ "$2" != "faxbox" ];
+then
+    ./process_logs.py --date $1
+fi
 sed -i "s/ES_INDEX/$3/" joblog.conf
 cat jobsarchived$1-processed.csv | /opt/logstash/bin/logstash -f joblog.conf
 rm *.csv
