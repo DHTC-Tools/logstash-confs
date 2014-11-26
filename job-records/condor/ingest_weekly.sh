7a03c6eaa56f31b629fd4f7e181e68115262d59d
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-11-26 12:55:28 -0600

    Give the cluster a little time to catch up after going through each log file

--- job-records/condor/ingest_weekly.sh
+++ job-records/condor/ingest_weekly.sh
@@ -6,6 +6,7 @@ cur_dir=`pwd`
 source=$1
 es_index=$2
 processed=$3
+sed -i "s/ES_INDEX/$es_index/" joblog.conf
 shift
 shift
 shift
@@ -13,14 +14,15 @@ while [ "$1" != "" ]; do
     if [ "$processed" == 'True' ];
     then
         ./download_logs.py --date $1 --source ${source} --processed
+        cat jobsarchived$1-processed.csv | /opt/logstash/bin/logstash -f joblog.conf
+        sleep 120
     else
         ./download_logs.py --date $1 --source ${source}
         ./process_logs.py --date $1
-
+        cat jobsarchived$1-processed.csv | /opt/logstash/bin/logstash -f joblog.conf
+        sleep 120
     fi
     shift
 done
 
-sed -i "s/ES_INDEX/$es_index/" joblog.conf
-cat *-processed.csv | /opt/logstash/bin/logstash -f joblog.conf
 rm *.csv
