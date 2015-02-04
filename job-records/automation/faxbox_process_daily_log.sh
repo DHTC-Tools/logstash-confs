3e54cd88b52e6d4ef0e36b75cffabdcc02e88932
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-02-04 03:19:15 -0600

    Updates to script to automate panda job download and processing

--- job-records/automation/faxbox_process_daily_log.sh
+++ job-records/automation/faxbox_process_daily_log.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 yesterday=`date --date="3 days ago" +"%Y%m%d"`
 year=`date --date="3 days ago" +"%Y"`
-faxbox_logs="/faxbox/group/logs/panda_logs/$year/"
-./generate_process_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/faxbox_logs
+faxbox_logs="/faxbox/group/logs/jobs"
+curr_dir=`pwd`
+cd $1
+./generate_process_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/faxbox_logs --data_source amazon
 cd /tmp/faxbox_logs
-echo '+Project="atlas-org-uchicago"' >> *.submit
+submit_file=`ls *.submit`
+sed 's/.*queue 1.*/+Project="atlas-org-uchicago"\n&/' $submit_file  > temp_file
+sed 's/.*queue 1.*/Requirements = regexp("uc3-c.*", Machine)\n&/' temp_file > $submit_file
 condor_submit *.submit
-mv jobsarchived$yesterday-processed.log  $faxbog_logs/processed
-mv jobsarchived$yesterday-error.log  $faxbog_logs/processed
-mv jobsarchived$yesterday.log  $faxbog_logs/raw
-cd
+condor_wait job_logs/*.log
+mv jobsarchived$yesterday-processed.csv  $faxbox_logs/processed/$year
+mv jobsarchived$yesterday-bad.csv  $faxbox_logs/processed/$year
+mv jobsarchived$yesterday.csv  $faxbox_logs/raw/$year
 rm -fr /tmp/faxbox_logs
+cd $curr_dir
