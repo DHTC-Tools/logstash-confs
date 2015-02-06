73e1c6d78d2be960879d92187213657b5d6076f9
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-02-06 11:46:27 -0600

    Fixes for updating ES with panda job records daily

--- job-records/automation/load_elasticsearch_daily.sh
+++ job-records/automation/load_elasticsearch_daily.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 yesterday=`date --date="3 days ago" +"%Y%m%d"`
 year=`date --date="3 days ago" +"%Y"`
-faxbox_logs="/faxbox/group/logs/panda_logs/$year/"
-./generate_ingest_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/ingest_logs
+cd $1
+./generate_ingest_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/ingest_logs --data_source faxbox
 cd /tmp/ingest_logs
 condor_submit *.submit
+condor_wait job_logs/*.log
 cd
 rm -fr /tmp/ingest_logs
