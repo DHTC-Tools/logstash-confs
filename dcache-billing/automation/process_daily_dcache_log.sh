2f0e5fafd59266128c5ac858425daf2810f12020
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-02-16 15:38:28 -0600

    Fix some logic issues and point to correct directory

--- dcache-billing/automation/process_daily_dcache_log.sh
+++ dcache-billing/automation/process_daily_dcache_log.sh
@@ -3,7 +3,7 @@ yesterday=`date --date="yesterday" +"%Y.%m.%d"`
 year=`date --date="yesterday" +"%Y"`
 month=`date --date="yesterday" +"%m"`
 billing_logs="/var/lib/dcache/billing/$year/$month"
-log_dest="/mnt/log/mwt2/dcache-billing/"
+log_dest="/mnt/logs/mwt2/dcache-billing/"
 work_dir=`mktemp -d`
 cd $work_dir
 mkdir logs
@@ -15,8 +15,9 @@ cd logs
 sha256sum billing* > raw_sums
 cd ../processed_logs
 sha256sum billing* > processed_sums
+cd ..
 cp logs/billing* $log_dest/raw/$year
-cp processed_logs/billing* $log_dest/raw/$year
+cp processed_logs/billing* $log_dest/processed/$year
 cat logs/raw_sums >> $log_dest/raw/$year/sha256sums
 cat processed_logs/processed_sums >> $log_dest/processed/$year/sha256sums
 rm -fr $work_dir
