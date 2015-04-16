e16d92e973570b22bd0955694d54325bb73d5a20
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-04-16 15:01:31 -0500

    Make sure to use processed records

--- job-records/automation/load_elasticsearch_daily.sh
+++ job-records/automation/load_elasticsearch_daily.sh
@@ -2,7 +2,7 @@
 yesterday=`date --date="3 days ago" +"%Y%m%d"`
 year=`date --date="3 days ago" +"%Y"`
 cd $1
-./generate_ingest_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/ingest_logs --data_source faxbox
+./generate_ingest_submit.py --startdate $yesterday --enddate $yesterday --location /tmp/ingest_logs --data_source faxbox  --processed
 cd /tmp/ingest_logs
 condor_submit *.submit
 condor_wait job_logs/*.log
