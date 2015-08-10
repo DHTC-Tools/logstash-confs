e87e60a8837b9703a819b67366bc3ef1ac40f206
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-08-10 11:21:22 -0500

    Don't pad month and day

--- dcache-billing/automation/optimize_dcache_index.sh
+++ dcache-billing/automation/optimize_dcache_index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 year=`date --date="yesterday" +"%Y"`
-month=`date --date="yesterday" +"%m"`
-day=`date --date="yesterday" +"%d"`
+month=`date --date="yesterday" +"%-m"`
+day=`date --date="yesterday" +"%-d"`
 isoweek=`python -c "import datetime; print datetime.date($year, $month, $day).isocalendar()[1]"`
 index="billing_"$year"_$isoweek"
 curl -m 1800 -XPOST "http://uct2-es-door.mwt2.org:9200/$index/_optimize?max_num_segments=1"
